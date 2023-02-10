5b5fd50dd64b99c5317ca70dee0752210c12143c
xia0 <cpt.acky@gmail.com>
2023-02-11 00:21:47 +1030

    initial commit

---
+++ keyboards/sauceball/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEE, 0x94, 0x7E, 0x3C, 0x37, 0x82, 0x30, 0xA3}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0, 0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1, 2, 3}
