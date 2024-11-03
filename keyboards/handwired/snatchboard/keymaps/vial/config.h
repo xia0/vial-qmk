317a896965cac5484af8ed09658b6bb745fd6b1e
xia0 <cpt.acky@gmail.com>
2024-11-04 00:03:01 +1100

    first commit

---
+++ keyboards/handwired/snatchboard/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2022 xia0 (@xia0)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x60, 0xD1, 0x62, 0xF5, 0x38, 0x04, 0x05, 0xA3}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
