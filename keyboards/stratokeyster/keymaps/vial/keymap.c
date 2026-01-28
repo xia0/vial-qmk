39e1c20930a6c2345355027e8b96d836d6c16a83
xia0 <cpt.acky@gmail.com>
2026-01-28 14:59:43 +1100

    fixing keymap

--- keyboards/stratokeyster/keymaps/vial/keymap.c
+++ keyboards/stratokeyster/keymaps/vial/keymap.c
@@ -47,7 +47,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SPC , KC_SPC ,
     KC_SPC , KC_SPC ,
     KC_LWIN, KC_LALT, _______, KC_LCTL, KC_LSFT,
-
     KC_MUTE,
     KC_LCAP, _______, _______, _______
   ),
