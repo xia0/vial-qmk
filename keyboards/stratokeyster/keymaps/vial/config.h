5a6edc39f85980240791a15c48b2a02ea1b953e6
xia0 <cpt.acky@gmail.com>
2026-04-07 12:13:18 +1000

    changed eeprom driver back to rp2040

--- keyboards/stratokeyster/keymaps/vial/config.h
+++ keyboards/stratokeyster/keymaps/vial/config.h
@@ -1,7 +1,5 @@
 #pragma once
-
-#define TRANSIENT_EEPROM_SIZE 8192
-#define DYNAMIC_KEYMAP_LAYER_COUNT 31
+#define DYNAMIC_KEYMAP_LAYER_COUNT 32
 
 #define VIAL_KEYBOARD_UID {0x9C, 0x9C, 0xD6, 0x66, 0xE3, 0xB1, 0x46, 0x0B}
 /* Vial unlock keycombo: up pick on all 3 strum bars */
